3b72b4fe6a397e48b13f647bd14f3b68861e746e
Jake (xczs) <112898008+xczk@users.noreply.github.com>
2022-12-18 21:03:15 +0100

    Update report-status.js

--- source/commands/report-status.js
+++ source/commands/report-status.js
@@ -1,1 +1,17 @@
+const snekfetch = require('snekfetch');
 
+module.exports = {
+    name: 'report',
+    description: 'can report the status of what you would like',
+    execute(message, args) {
+      snekfetch.get('https://pastebin.com/raw/yourpastebinlol').then(e => {
+          let dfgiij = new discord.MessageEmbed()
+          .setTimestamp()
+          .setTitle(`M1r3 Cybersec`)
+          .setDescription(`
+          ${e.body}
+          `);
+      message.channel.send(dfgiij); 
+      });
+    }
+}
